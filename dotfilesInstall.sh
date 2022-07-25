b14d672bbcc6da76802ca6bae6c57f4a4a19267c
Denis <denistroobants@gmail.com>
2022-07-25 12:11:25 +0200

    Update dotfilesInstall.sh

--- dotfilesInstall.sh
+++ dotfilesInstall.sh
@@ -1,6 +1,6 @@
-git clone --bare https://bitbucket.org/durdn/cfg.git $HOME/.cfg
+git clone --bare git@github.com:dstroobants/dotfiles.git $HOME/.dotfiles
 function dotfiles {
-   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
 mkdir -p .dotfiles-backup
 dotfiles checkout
